c38921909cc3f245aa74bc298677ae6c3d3222e1
Baraban <bara_ban@mail.ru>
2012-12-29 13:27:43 +0400

    + backup ubuntu system settings

---
+++ scripts/restore-system-conf.sh
@@ -0,0 +1,13 @@
+#dpkg --get-selections > ~/Package.list
+#sudo cp /etc/apt/sources.list ~/sources.list
+#sudo apt-key exportall > ~/Repo.keys
+#rsync --progress /home/`whoami` /path/to/user/profile/backup/here
+
+##  Reinstall now
+
+rsync --progress /path/to/user/profile/backup/here /home/`whoami`
+sudo apt-key add ~/Repo.keys
+sudo cp ~/sources.list /etc/apt/sources.list 
+sudo apt-get install dselect
+sudo dpkg --set-selections < ~/Package.list
+sudo dselect
