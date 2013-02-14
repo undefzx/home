cf3cf13505f4f64837b40328fa2ca548c2f6afd1
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2013-02-14 20:10:56 +0400

    + bash setting & setup

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 ln -s ~/home/.vimrc ~/.vimrc
 ln -s ~/home/.gitignore ~/.gitignore
+ln -s ~/home/.bash_user ~/.bash_user
+echo ". ~/.bash_user" >> ~/.bashrc
 git config --global core.excludesfile ~/home/.gitignore
 git config --global color.ui "auto"
 git config --global user.name "Dmitriy Boyarshinov"
