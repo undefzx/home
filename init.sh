2339a46881642178ed42ef690aa761b53cbe9064
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2013-02-14 17:21:17 +0400

    игнорировать *.pyc

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
-ln -s ~/home/.vimrc ~/.vimrc
+#ln -s ~/home/.vimrc ~/.vimrc
+ln -s ~/home/.gitignore ~/.gitignore
+git config --global core.excludesfile ~/home/.gitignore
+git config --global color.ui "auto"
+git config --global user.name "Dmitriy Boyarshinov"
+git config --global user.email "dmitriy.boyarshinov@gmail.com"
+#git config --global apply.whitespace nowarn
