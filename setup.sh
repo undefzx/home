726eaf8e4d1ec5a5224420513ae2c869e438a5aa
undefzx <bara_ban@mail.ru>
2016-10-18 16:01:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-#general
+# general
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install git vim ctags
 
-#python
+# devel
+sudo apt-get install linux-headers-$(uname -r) build-essential
+
+# python
 sudo apt-get python-pip
+sudo apt-get install python2.7-dev
 
 # virtualenv
 sudo pip install virtualenv
