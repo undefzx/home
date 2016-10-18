3d15d50a999387b8eb7f44fb5478ad40405e077c
undefzx <bara_ban@mail.ru>
2016-10-18 15:44:33 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#general
+sudo apt-get install git vim ctags
+
+#python
+sudo apt-get python-pip
+
+# virtualenv
+sudo pip install virtualenv
+mkdir ~/.virtualenvs
+sudo pip install virtualenvwrapper
+export WORKON_HOME=~/.virtualenvs
